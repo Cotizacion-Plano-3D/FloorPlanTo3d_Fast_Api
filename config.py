410bee16e6e68ea4a81afac3e0b43de7503ecfee
AlexTiclla <58306419+AlexTiclla@users.noreply.github.com>
2025-11-20 10:23:20 -0400

    C:/Program Files/Git/ quitado

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     STRIPE_SECRET_KEY: str
     STRIPE_WEBHOOK_SECRET: str
     FRONTEND_URL: str = "https://floorplanto3dfrontendreact-eight.vercel.app"  # URL del frontend
-    FLOORPLAN_API_URL: str = "https://floorplanto3dapi-production.up.railway.app/"  # URL del servicio Flask
+    FLOORPLAN_API_URL: str = "https://floorplanto3dapi-production.up.railway.app"  # URL del servicio Flask
     GOOGLE_DRIVE_FOLDER_ID: str = "1_Mv_vpgc-0LCEuPaI49Ym3xvzvRhW7OW"  # ID del folder de Google Drive
     GOOGLE_CREDENTIALS_PATH: str = "./credentials.json"
     GOOGLE_OAUTH_REDIRECT_URI: str = "http://localhost:8000/auth/google/callback"
